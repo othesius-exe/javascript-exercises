eded4395cde2508075e6ac74865f1d1b9107766d
Caleb Imler <calebqimler@gmail.com>
2020-06-14 15:36:02 -0400

    Having trouble with negative temps

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  celsiusTemp = (temp - 32) * .556;
+  return roundedToFixed(celsiusTemp, 1);
 }
 
-const ctof = function() {
+const ctof = function(temp) {
+    fahrenheitTemp = (temp * 1.8) + 32;
+    return roundedToFixed(fahrenheitTemp, 1);
+}
 
+function roundedToFixed(decimal, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return (Math.round(decimal * multiplier) / multiplier);
 }
 
 module.exports = {
